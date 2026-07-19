097b47b343f049707f413934242e59b3071bdd67
Mmesoma Kenechi <ndukammesomakenechi@gmail.com>
2026-07-19 09:26:13 +0100

    Add custom webpack alias configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname)
+    return config
   },
 }
 
